3ad88964b99009dc30bfadafc31543f45e8ba368
Cameron Aragon <caragon4695@gmail.com>
2020-12-07 15:15:03 -0700

    Add chai and mocha testing syntax

---
+++ test/User-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require('../src/User');
+
+describe('User', () => {
+
+});
---
+++ test/UserRepository-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const UserRepository = require('../src/UserRepository');
+
+describe('UserRepository', () => {
+
+});
