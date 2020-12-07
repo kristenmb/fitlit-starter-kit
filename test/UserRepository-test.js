ea6ba1b663663838930dd9b269874c7e6b920386
Kristen Bair <kristenmbair@gmail.com>
2020-12-07 15:56:08 -0700

    Add psuedocode for testing and class structure

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -6,3 +6,17 @@ const UserRepository = require('../src/UserRepository');
 describe('UserRepository', () => {
 
 });
+
+
+//holds all user objects
+//should have a parameter to take in user data
+// // // data parameter is the userdata array of objects.
+// TEST: an instantiation of UserRepository has a property that holds the array of user info
+
+//should have a method to:
+// // // gievn user id what is their user data
+// // // // user data will be their id, name, address, email, strideLength, dailyStepGoal, friends lists
+// // // // // TEST: put in specific id, return correct object of data for that user
+// // // average step goal amongst all users
+// // // // .map() or .reduce() to return average step goal of all users
+// // // // // TEST: calling the method returns the average step count
