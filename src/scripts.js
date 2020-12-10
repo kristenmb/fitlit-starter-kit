5936888eea57c857c4ceebabfe5e4419b6a6c84d
Cameron Aragon <caragon4695@gmail.com>
2020-12-09 18:52:55 -0700

    Add class instantiations and DOM selectors to display user's name

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const allUsers = new UserRepository(userData);
+const currentUser = new User(allUsers.accessUser(6));
+const greeting = document.querySelector('#greeting');
+
+window.addEventListener('load', displayUserInfo);
+
+function displayUserInfo() {
+    displayGreeting();
+}
+
+function displayGreeting() {
+    greeting.innerText = `Welcome, ${currentUser.accessUserName()}`;
+};
