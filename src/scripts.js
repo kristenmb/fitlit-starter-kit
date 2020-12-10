b257d859d0e95dbe63a3a367406f4e0e0c44b61c
Cameron Aragon <caragon4695@gmail.com>
2020-12-09 19:09:14 -0700

    Add current user's info to the user info card in the DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,27 @@
 const allUsers = new UserRepository(userData);
 const currentUser = new User(allUsers.accessUser(6));
 const greeting = document.querySelector('#greeting');
+const userInfo = document.querySelector('#user-info');
 
-window.addEventListener('load', displayUserInfo);
+window.addEventListener('load', displayUserProfile);
 
-function displayUserInfo() {
+function displayUserProfile() {
     displayGreeting();
-}
+    displayUserInfo();
+};
 
 function displayGreeting() {
     greeting.innerText = `Welcome, ${currentUser.accessUserName()}`;
+};
+
+function displayUserInfo() {
+    userInfo.innerHTML = `
+        <div class="side">
+            <p>${currentUser.address}</p>
+            <p>${currentUser.email}</p>
+        </div>
+        <div class="side">
+            <p>Stride Length: ${currentUser.strideLength}</p>
+            <p>Your Step Goal: ${currentUser.dailyStepGoal} steps</p>
+        </div>`
 };
