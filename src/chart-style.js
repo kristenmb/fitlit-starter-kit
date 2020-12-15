7a868f556570ed7a4bd5ccc68a698957ce0382da
Kristen Bair <kristenmbair@gmail.com>
2020-12-15 16:54:14 -0700

    Add charts for every weekly statistic display

--- src/chart-style.js
+++ src/chart-style.js
@@ -8,8 +8,14 @@ const activityRepo1 = new ActivityRepo(activityData);
 const weeklyStepsGraph = document.querySelector('#wkly-step-graph');
 const weeklyFlightsGraph = document.querySelector('#wkly-flights-graph');
 const weeklyMinsActiveGraph = document.querySelector('#wkly-mins-active-graph');
+const weeklyWaterGraph = document.querySelector('#wkly-water-graph');
+const weeklyHrsSleptGraph = document.querySelector('#wkly-hrs-graph');
+const weeklyQltyGraph = document.querySelector('#wkly-qlty-graph');
 
 const stepsChart = new Chart(weeklyStepsGraph, {
+    axisX: {
+      valueFormatString: 'DD-MMM'
+    },
     type: 'line',
     data: {
     labels: Object.keys(activity1.createWeeklyActivityLog(activity1.data[activity1.data.length - 7].date, 'numSteps')),
@@ -54,6 +60,45 @@ const activeChart = new Chart(weeklyMinsActiveGraph, {
         label: 'Weekly Minutes Active',
         borderColor: 'rgb(255, 245, 0)',
         data: Object.values(activity1.createWeeklyActivityLog(activity1.data[activity1.data.length - 7].date, 'minutesActive'))
+      }]
+    },
+  options: {}
+});
+
+const waterChart = new Chart(weeklyWaterGraph, {
+    type: 'line',
+    data: {
+    labels: Object.keys(hydration1.createWeeklyHydrationLog(hydration1.data[hydration1.data.length - 7].date)),
+    datasets: [{
+        label: 'Weekly Water',
+        borderColor: 'rgb(0, 236, 255)',
+        data: Object.values(hydration1.createWeeklyHydrationLog(hydration1.data[hydration1.data.length - 7].date))
+      }]
+    },
+  options: {}
+});
+
+const sleepHrsChart = new Chart(weeklyHrsSleptGraph, {
+    type: 'line',
+    data: {
+    labels: Object.keys(sleep1.createWeeklySleepLog(sleep1.data[sleep1.data.length - 7].date, 'hoursSlept')),
+    datasets: [{
+        label: 'Weekly Hours',
+        borderColor: 'rgb(200, 90, 253)',
+        data: Object.values(sleep1.createWeeklySleepLog(sleep1.data[sleep1.data.length - 7].date, 'hoursSlept'))
+      }]
+    },
+  options: {}
+});
+
+const qualityChart = new Chart(weeklyQltyGraph, {
+    type: 'line',
+    data: {
+    labels: Object.keys(sleep1.createWeeklySleepLog(sleep1.data[sleep1.data.length - 7].date, 'sleepQuality')),
+    datasets: [{
+        label: 'Weekly Hours',
+        borderColor: 'rgb(200, 90, 253)',
+        data: Object.values(sleep1.createWeeklySleepLog(sleep1.data[sleep1.data.length - 7].date, 'sleepQuality'))
       }]
     },
   options: {}
--- src/scripts.js
+++ src/scripts.js
@@ -8,11 +8,8 @@ const greeting = document.querySelector('#greeting');
 const userInfo = document.querySelector('#user-info');
 const compareStep = document.querySelector('#compare-step');
 const dailyWater = document.querySelector('#dly-water');
-const weeklyWater = document.querySelector('#weekly-water');
 const dailySleepHrs = document.querySelector('#hrs-slept');
 const dailySleepQlty = document.querySelector('#sleep-qlty');
-const weeklySleepLog = document.querySelector('#wkly-sleep');
-const weeklySleepQlty = document.querySelector('#wkly-qlty');
 const allTimeSleepAvg = document.querySelector('#all-time-sleep');
 const allTimeQltyAvg = document.querySelector('#all-time-quality');
 const dailySteps = document.querySelector('#dly-steps');
@@ -21,23 +18,7 @@ const milesWalked = document.querySelector('#miles-walked');
 const compareDailySteps = document.querySelector('#compare-avg-steps');
 const compareActivityMinutes = document.querySelector('#compare-avg-min');
 const compareFlights = document.querySelector('#compare-avg-flights');
-const weeklySteps = document.querySelector('#wkly-steps');
-const weeklyFlights = document.querySelector('#wkly-flights');
-const weeklyMinActive = document.querySelector('#wkly-min-active');
 
-// const weeklyStepsGraph = document.querySelector('#wkly-step-graph');
-// const chart = new Chart(weeklyStepsGraph, {
-//     type: 'line',
-//     data: {
-//     labels: Object.keys(activity.createWeeklyActivityLog(activity.data[activity.data.length - 7].date, 'numSteps')),
-//     datasets: [{
-//         label: 'Weekly Step Graph',
-//         borderColor: 'rgb(1, 255, 0)',
-//         data: Object.values(activity.createWeeklyActivityLog(activity.data[activity.data.length - 7].date, 'numSteps'))
-//       }]
-//     },
-//   options: {}
-// });
 
 window.addEventListener('load', displayUserProfile);
 
@@ -45,19 +26,13 @@ function displayUserProfile() {
   displayGreeting();
   displayUserInfo();
   displayDailyWater();
-  displayWeeklyWater();
   displayDailyHoursSlept();
   displayDailySleepQuality();
-  displayWeeklySleepLog('hoursSlept');
-  displayWeeklySleepLog('sleepQuality');
   displayAllTimeSleepInfo();
   displayDailySteps();
   displayActiveMins();
   displayMilesWalked();
   displayCommunityAvg();
-  // displayWeeklyStepLog('numSteps');
-  // displayWeeklyStepLog('flightsOfStairs');
-  displayWeeklyStepLog('minutesActive');
 };
 
 function displayGreeting() {
@@ -80,40 +55,12 @@ function displayDailyWater() {
   dailyWater.innerText = `${hydration.findOuncesByDate(hydration.data[hydration.data.length - 1].date)} OZ`;
 }
 
-function displayWeeklyWater() {
-  const weeklyWaterLog = hydration.createWeeklyHydrationLog(hydration.data[hydration.data.length - 7].date)
-  const dates = Object.keys(weeklyWaterLog);
-  const ounces = Object.values(weeklyWaterLog);
-  const waterList = dates.map((each, i) => each + ': ' + ounces[i]);
-  weeklyWater.innerHTML = `
-    <p>${waterList[0]} <br> ${waterList[1]} <br> ${waterList[2]} <br> ${waterList[3]}  <br> ${waterList[4]}
-    <br> ${waterList[5]}  <br> ${waterList[6]}</p>`;
-}
-
 function displayDailyHoursSlept() {
   dailySleepHrs.innerText = `${sleep.findByDate((sleep.data[sleep.data.length - 1].date), 'hoursSlept')}`
 }
 
 function displayDailySleepQuality() {
   dailySleepQlty.innerText = `${sleep.findByDate((sleep.data[sleep.data.length - 1].date), 'sleepQuality')}`
-}
-
-function displayWeeklySleepLog(property) {
-  const weeklySleep = sleep.createWeeklySleepLog(sleep.data[sleep.data.length - 7].date, property);
-  const dates = Object.keys(weeklySleep);
-  const values = Object.values(weeklySleep);
-  const sleepList = dates.map((each, i) => each + ': ' + values[i]);
-  if (property === 'hoursSlept') {
-    weeklySleepLog.innerHTML = `
-      <p>WEEKLY HRS SLEPT</p>
-      <p>${sleepList[0]} <br> ${sleepList[1]} <br> ${sleepList[2]} <br> ${sleepList[3]}  <br> ${sleepList[4]}
-      <br> ${sleepList[5]}  <br> ${sleepList[6]}</p>`;
-  } else {
-    weeklySleepQlty.innerHTML = `
-      <p>WEEKLY SLEEP QUALITY</p>
-      <p>${sleepList[0]} <br> ${sleepList[1]} <br> ${sleepList[2]} <br> ${sleepList[3]}  <br> ${sleepList[4]}
-      <br> ${sleepList[5]}  <br> ${sleepList[6]}</p>`;
-  }
 }
 
 function displayAllTimeSleepInfo() {
@@ -136,33 +83,7 @@ function displayMilesWalked() {
 
 function displayCommunityAvg() {
   compareStep.innerText = `${allUsers.findAllUserSteps()}`;
-
   compareDailySteps.innerText = `${activityRepo.findDailyAvg('numSteps', activityRepo.data[activityRepo.data.length - 1].date)}`;
-
   compareActivityMinutes.innerText = `${activityRepo.findDailyAvg('minutesActive', activityRepo.data[activityRepo.data.length - 1].date)}`;
-
   compareFlights.innerText = `${activityRepo.findDailyAvg('flightsOfStairs', activityRepo.data[activityRepo.data.length - 1].date)}`;
-}
-
-function displayWeeklyStepLog(property) {
-  const weeklyActive = activity.createWeeklyActivityLog(activity.data[activity.data.length - 7].date, property);
-  const dates = Object.keys(weeklyActive);
-  const values = Object.values(weeklyActive);
-  const activityList = dates.map((each, i) => each + ': ' + values[i]);
-  if (property === 'numSteps') {
-    weeklySteps.innerHTML = `
-      <p>WEEKLY STEPS</p>
-      <p>${activityList[0]} <br> ${activityList[1]} <br> ${activityList[2]} <br> ${activityList[3]}  <br> ${activityList[4]}
-      <br> ${activityList[5]}  <br> ${activityList[6]}</p>`;
-  } else if (property === 'flightsOfStairs') {
-    weeklyFlights.innerHTML = `
-      <p>WEEKLY FLIGHTS OF STAIRS</p>
-      <p>${activityList[0]} <br> ${activityList[1]} <br> ${activityList[2]} <br> ${activityList[3]}  <br> ${activityList[4]}
-      <br> ${activityList[5]}  <br> ${activityList[6]}</p>`;
-  } else {
-    weeklyMinActive.innerHTML = `
-      <p>WEEKLY MINUTES ACTIVE</p>
-      <p>${activityList[0]} <br> ${activityList[1]} <br> ${activityList[2]} <br> ${activityList[3]}  <br> ${activityList[4]}
-      <br> ${activityList[5]}  <br> ${activityList[6]}</p>`;
-  }
 }
