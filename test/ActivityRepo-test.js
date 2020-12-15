2bb28634f72c397ba41406b16b707580e39ff318
Kristen Bair <kristenmbair@gmail.com>
2020-12-14 19:00:53 -0700

    Add functionality to calculate average steps, flights of stairs, min active for all users on a given date

--- test/ActivityRepo-test.js
+++ test/ActivityRepo-test.js
@@ -18,5 +18,17 @@ describe('ActivityRepo', () => {
 
   it('should take in activity data', () => {
     expect(activityRepo.data).to.deep.equal(activityData);
-  })
-})
+  });
+
+  it('should be able to find average number of stairs climbed for all users on a specific day', () => {
+    expect(activityRepo.findDailyAvg('flightsOfStairs', '2019/06/15')).to.equal('24');
+  });
+
+  it('should be able to find average number of steps taken for all users on a specific day', () => {
+    expect(activityRepo.findDailyAvg('numSteps', '2019/06/15')).to.equal('8386');
+  });
+
+  it('should be able to find average minutes active for all users on a specific day', () => {
+    expect(activityRepo.findDailyAvg('minutesActive', '2019/06/15')).to.equal('156');
+  });
+});
